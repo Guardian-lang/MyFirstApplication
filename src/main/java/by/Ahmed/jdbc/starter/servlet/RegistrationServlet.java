f37fd1932db0168e0e2eaf75a448cf6ebe37e2e3
Ahmed <83308467+Guardian-lang@users.noreply.github.com>
2023-04-14 19:09:46 +0300

    MyFirstApplication

--- src/main/java/by/Ahmed/jdbc/starter/servlet/RegistrationServlet.java
+++ src/main/java/by/Ahmed/jdbc/starter/servlet/RegistrationServlet.java
@@ -3,7 +3,9 @@ package by.Ahmed.jdbc.starter.servlet;
 import by.Ahmed.jdbc.starter.dto.CreateUserDto;
 import by.Ahmed.jdbc.starter.entity.Gender;
 import by.Ahmed.jdbc.starter.entity.Role;
+import by.Ahmed.jdbc.starter.service.UserService;
 import by.Ahmed.jdbc.starter.utils.JspHelper;
+import by.Ahmed.jdbc.starter.validator.ValidationException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -14,6 +16,8 @@ import java.io.IOException;
 
 @WebServlet("/registration")
 public class RegistrationServlet extends HttpServlet {
+
+    private final UserService userService = UserService.getInstance();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setAttribute("roles", Role.values());
@@ -33,5 +37,12 @@ public class RegistrationServlet extends HttpServlet {
                 .role(req.getParameter("role"))
                 .gender(req.getParameter("gender"))
                 .build();
+        try {
+            userService.create(userDto);
+            resp.sendRedirect("/login");
+        } catch (ValidationException exception) {
+            req.setAttribute("errors", exception.getErrors());
+            doGet(req, resp);
+        }
     }
 }
